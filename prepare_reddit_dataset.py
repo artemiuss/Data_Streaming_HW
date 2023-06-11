5fdb2a48664eaae5ee4734ca1c2b3f58148c66a3
Artem Avetian <artemiustas@gmail.com>
2023-06-12 01:05:48 +0300

    changes

--- prepare_reddit_dataset.py
+++ prepare_reddit_dataset.py
@@ -2,6 +2,9 @@
 import praw, random
 from langdetect import detect
 from iso639 import languages
+from multi_rake import Rake
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 def main():
     """
@@ -15,21 +18,36 @@ def main():
         username = "Brilliant_Cup_3679",
     )
 
+    rake = Rake()
+
+    nltk.download('vader_lexicon')
+    # Initialize the sentiment analyzer
+    sid = SentimentIntensityAnalyzer()
+
     while True:
         try:
             comments = reddit.random_subreddit().comments(limit=25)
             comments_list = list(comments)
             random_comment = random.choice(comments_list)
-            print(random_comment)
+            #print(random_comment)
+            print(random_comment.body)
+            #print(random_comment.controversiality)
+            #print(random_comment.score)
+            #print(random_comment.polarity)
             detect_result = detect(random_comment.body)
             detect_result = languages.get(part1=detect_result)
-            print(detect_result.name)
+            #print(detect_result.name)
+            #if detect_result.name != "English":
+            #    print(detect_result.name)
+            
+            keywords = rake.apply(random_comment.body)
+            print(f"keywords= {keywords[:10]}")
+
+            # Analyze sentiment
+            sentiment_scores = sid.polarity_scores(random_comment.body)
+            print(f"sentiment_scores=  {sentiment_scores}")
         except Exception as e:
             pass
-
-# TODO
-# Sentiment Analysis
-# Keyword Analysis
 
 if __name__ == '__main__':
     main()
