ffdc3e9b3871262fecbcc8cb64ffbe50aafc59b2
Artem Avetian <artemiustas@gmail.com>
2023-06-12 13:49:07 +0300

    changes

---
+++ consumer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os, logging
+from kafka import KafkaConsumer
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
---
+++ reporter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import os, logging
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
