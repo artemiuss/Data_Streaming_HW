44c054af627b65cf6af314b378c1474ca7d83e41
Artem Avetian <artemiustas@gmail.com>
2023-06-12 16:12:20 +0300

    changes

--- producer.py
+++ producer.py
@@ -10,14 +10,14 @@ def main():
     with open(filepath, encoding="utf8") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            print(row)
+            #print(row)
             json_data = json.dumps(row).encode('utf-8')
-            print(json_data)
-            #producer.poll(0)
+            #print(json_data)
             #row['producer_timestamp'] = int(time.time() * 1000)
             
             producer.send(topic="reddit_ds", value=json_data)
-            #producer.flush()
-            
+        producer.flush()
+    producer.close()
+
 if __name__ == '__main__':
     main()
