6ca5929d53ba7b02ec49c97d48631fa22a71a118
Artem Avetian <artemiustas@gmail.com>
2023-06-15 16:27:37 +0300

    changes

--- producer.py
+++ producer.py
@@ -21,8 +21,7 @@ def main():
     with open(DS_FILENAME, encoding="utf8", newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
-            row['created'] = int(time.time())
-            #row['created'] = int(float(row['created']))
+            row['created'] = int(float(row['created']))
             json_data = json.dumps(row).encode('utf-8')
             print(json_data)
             producer.send(topic=KAFKA_TOPIC, value=json_data)
