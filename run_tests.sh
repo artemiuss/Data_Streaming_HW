1a4c8135e7aae00ecb8e8a5320af7366f712d291
Artem Avetian <artemiustas@gmail.com>
2023-06-18 16:18:44 +0300

    changes

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,9 @@
 
 # 1. One producer, a topic with one partition, one consumer
 printf "1. One producer, a topic with one partition, one consumer\n"
-PRODUCERS=1
-PARTITIONS=1
-CONSUMERS=1
+export PRODUCERS=1
+export PARTITIONS=1
+export CONSUMERS=1
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -16,9 +16,10 @@ docker compose down
 
 # 2. One producer, a topic with one partition, 2 consumers
 printf "2. One producer, a topic with one partition, 2 consumers\n"
-PRODUCERS=1
-PARTITIONS=1
-CONSUMERS=2
+export PRODUCERS=1
+export PARTITIONS=1
+export CONSUMERS=2
+
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
 printf "CONSUMERS: ${CONSUMERS}\n"
@@ -29,9 +30,9 @@ docker compose down
 
 # 3. One producer, a topic with 2 partitions, 2 consumers
 printf "3. One producer, a topic with 2 partitions, 2 consumers\n"
-PRODUCERS=1
-PARTITIONS=2
-CONSUMERS=2
+export PRODUCERS=1
+export PARTITIONS=2
+export CONSUMERS=2
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -43,9 +44,9 @@ docker compose down
 
 # 4. One producer, a topic with 5 partitions, 5 consumers
 printf "4. One producer, a topic with 5 partitions, 5 consumers\n"
-PRODUCERS=1
-PARTITIONS=5
-CONSUMERS=5
+export PRODUCERS=1
+export PARTITIONS=5
+export CONSUMERS=5
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -57,9 +58,9 @@ docker compose down
 
 # 5. One producer, a topic with 10 partitions, 1 consumers
 printf "5. One producer, a topic with 10 partitions, 1 consumers\n"
-PRODUCERS=1
-PARTITIONS=10
-CONSUMERS=1
+export PRODUCERS=1
+export PARTITIONS=10
+export CONSUMERS=1
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -71,9 +72,9 @@ docker compose down
 
 # 6. One producer, a topic with 10 partitions, 5 consumers
 printf "6. One producer, a topic with 10 partitions, 5 consumers\n"
-PRODUCERS=1
-PARTITIONS=10
-CONSUMERS=5
+export PRODUCERS=1
+export PARTITIONS=10
+export CONSUMERS=5
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -85,9 +86,9 @@ docker compose down
 
 # 7. One producer, a topic with 10 partitions, 10 consumers
 printf "7. One producer, a topic with 10 partitions, 10 consumers\n"
-PRODUCERS=1
-PARTITIONS=10
-CONSUMERS=10
+export PRODUCERS=1
+export PARTITIONS=10
+export CONSUMERS=10
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
@@ -99,9 +100,9 @@ docker compose down
 
 # 8. 2 producers (input data should be split into 2 parts somehow), a topic with 10 partitions, 10 consumers
 printf "8. 2 producers (input data should be split into 2 parts somehow), a topic with 10 partitions, 10 consumers\n"
-PRODUCERS=2
-PARTITIONS=10
-CONSUMERS=10
+export PRODUCERS=2
+export PARTITIONS=10
+export CONSUMERS=10
 
 printf "PRODUCERS: ${PRODUCERS}\n"
 printf "PARTITIONS: ${PARTITIONS}\n"
