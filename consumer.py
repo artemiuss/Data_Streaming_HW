b5db1b4e3331b7fe16ad9eb4451fce47cb725aeb
Artem Avetian <artemiustas@gmail.com>
2023-06-20 12:59:01 +0300

    changes

--- consumer.py
+++ consumer.py
@@ -19,7 +19,7 @@ def consume(kafka_host, kafka_port, kafka_topic, pg_user, pg_password, pg_databa
     for message in consumer:
         print (f"partition={message.partition}, offset={message.offset}, key={message.key}, timestamp={message.timestamp}")
         #print (f"value={message.value}")
-        time.sleep(0.01)
+        time.sleep(0.001)
         created = int(message.value['created'])
         processed = int(datetime.datetime.utcnow().timestamp()*1e3)
         size = sys.getsizeof(json.dumps(message.value))
